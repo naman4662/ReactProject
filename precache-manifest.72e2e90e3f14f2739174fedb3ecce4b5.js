4ece373eea5edd33ba6522e82529939f6b33b4b3
naman.agarwal <naman.agarwal@kronos.com>
2020-03-23 16:49:36 +0530

    Updates

---
+++ precache-manifest.72e2e90e3f14f2739174fedb3ecce4b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b46b7f6941f96c83a44822276df9029e",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "72f5f4f4bfa3008ae112",
+    "url": "/ReactProject/static/css/main.084a1039.chunk.css"
+  },
+  {
+    "revision": "046e092a35e705892b08",
+    "url": "/ReactProject/static/js/2.8ba0a23c.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/ReactProject/static/js/2.8ba0a23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72f5f4f4bfa3008ae112",
+    "url": "/ReactProject/static/js/main.ef73f7bb.chunk.js"
+  },
+  {
+    "revision": "508ea2a50a1cf06e6b29",
+    "url": "/ReactProject/static/js/runtime-main.9ee72d4d.js"
+  }
+]);
